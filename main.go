54e647b9bf5bcea5eaf200867802d02152f4ce91
Dustin Sallings <dustin@spy.net>
2013-06-26 03:03:45 -0700

    Fix house path.

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ var routingTable []routingEntry = []routingEntry{
 	routingEntry{"bleu.west.spy.net", regexp.MustCompile("^/therm/"),
 		proxyHandler("/", "http://menudo:7777/therm/")},
 	routingEntry{"bleu.west.spy.net", regexp.MustCompile("^/house/"),
-		proxyHandler("/therm/", "http://menudo.west.spy.net:7777/")},
+		proxyHandler("/", "http://menudo.west.spy.net:7777/")},
 	routingEntry{"bleu.west.spy.net", regexp.MustCompile("^/gitmirror/"),
 		proxyHandler("/gitmirror/", "http://menudo:8124/")},
 	routingEntry{"bleu.west.spy.net", regexp.MustCompile("^/eve/"),
