f2780724c93ea36510a6faa62fdacbb317c0c9b8
Dustin Sallings <dustin@spy.net>
2014-12-24 18:12:33 -0800

    Stupid fix to previous change

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func (h *myHandler) ServeHTTP(ow http.ResponseWriter, req *http.Request) {
 		h.ch <- loggable{time.Now(), writer, req, orig, q}
 	}(req.URL.Path, req.URL.RawQuery)
 
-	route := findHandler(req.Host, req.URL.Path).Handler(writer, req)
+	findHandler(req.Host, req.URL.Path).Handler(writer, req)
 }
 
 func dropPrivs(uid, gid int, descriptors uint64) {
